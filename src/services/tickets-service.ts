4c970b9d44ca75691a0b277a4279609378ff174b
monique <moniquejp16@gmail.com>
2023-09-21 14:40:59 -0300

    feat: creando função para pegar os tickets, tickets-service

---
+++ src/services/tickets-service.ts
@@ -0,0 +1,11 @@
+import { Ticket } from '@prisma/client';
+import dayjs from 'dayjs';
+import { notFoundError } from '@/errors';
+
+async function ticketsGet(): Promise<Ticket> {
+
+    
+    const ticket = await ticketsRepository.ticketsGet();
+  
+    return ticket
+  }
