7f507a4bfca3ab8aafc70102545547615f258d85
monique <moniquejp16@gmail.com>
2023-09-22 03:04:26 -0300

    feat: criando função para pegar os tickets do usuario, tickets-controller

--- src/controllers/tickets-controller.ts
+++ src/controllers/tickets-controller.ts
@@ -3,7 +3,7 @@ import { ticketsService } from '@/services/tickets-service';
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 
-// essa função serve para pegar todos os tickrts
+// essa função serve para pegar todos os tickts, ingresos
 export async function ticketsGet(req: Request, res: Response) {
 
   const ticket = await ticketsService.ticketsGet();
@@ -11,3 +11,13 @@ export async function ticketsGet(req: Request, res: Response) {
   return res.status(httpStatus.CREATED).send(ticket)
   
 }
+
+// essa função serve para pegar todos os tickts do usuario
+export async function userTicketsGet(req: Request, res: Response) {
+
+  const ticket = await ticketsService.userTicketsGet();
+
+  return res.status(httpStatus.CREATED).send(ticket)
+  
+}
+
