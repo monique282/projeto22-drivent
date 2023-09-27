d31d05c80aded024da1d38d0982dc774cade51d2
monique <moniquejp16@gmail.com>
2023-09-27 20:11:43 -0300

    testando

--- src/controllers/test.funcionando/index.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-import httpStatus from 'http-status';
-
-export async function singInPost(req: Request, res: Response) {
-  return res.status(httpStatus.OK).send(200);
-}
